cae13422158a00f0977d7a9a191acf2460f6c69d
sumeetvaidya <esumeet@gmail.com>
2021-11-13 23:57:05 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -771,6 +771,7 @@ def main():
     st.set_page_config(layout='wide')
     show_header()
     holdings_df = get_holdings()
+    holdings_df.reset_index(inplace=True)
     show_holdings(holdings_df)
     if st.button('Start Analysis'):
         with st.spinner("Analysis Running"):
